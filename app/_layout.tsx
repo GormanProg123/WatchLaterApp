b9fc2ee0ca2e0d756d95a9712f0b3f9509eef9b0
GormanProg123 <gparfenchik2005@gmail.com>
2026-05-23 14:24:30 +0200

    [main ping]

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import {
 } from "@expo-google-fonts/inter";
 import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import * as SplashScreen from "expo-splash-screen";
+import { api } from "../api/client";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -23,6 +24,15 @@ export default function RootLayout() {
 
   useEffect(() => {
     SplashScreen.hideAsync();
+  }, []);
+
+  useEffect(() => {
+    const ping = () => api.get("/auth/ping").catch(() => {});
+
+    ping();
+
+    const interval = setInterval(ping, 14 * 60 * 1000);
+    return () => clearInterval(interval);
   }, []);
 
   return <Stack screenOptions={{ headerShown: false }} />;
